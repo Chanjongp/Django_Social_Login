c2fd40de828a5fcc2d0d6e750eceaa9ee4ceefa3
Chanjongp <pcj0198@daum.net>
2021-05-26 01:54:40 +0900

    Refacot : Github Login Refactoring

--- accounts/views.py
+++ accounts/views.py
@@ -1,24 +1,20 @@
+import requests
 from django.shortcuts import redirect
 from django.conf import settings
+from django.utils.translation import gettext_lazy as _
+from django.http import JsonResponse
+from json.decoder import JSONDecodeError
+from rest_framework import status
+from rest_framework.response import Response
 from dj_rest_auth.registration.views import SocialLoginView
 from allauth.socialaccount.providers.google import views as google_view
 from allauth.socialaccount.providers.kakao import views as kakao_view
 from allauth.socialaccount.providers.github import views as github_view
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
-import requests
-from rest_framework_simplejwt.token_blacklist.models import BlacklistedToken
-from django.http import JsonResponse
-from .models import User
-import json
-from json.decoder import JSONDecodeError
-from rest_framework import status
-from rest_framework.response import Response
-from rest_framework_simplejwt.tokens import RefreshToken, OutstandingToken
-from rest_framework_simplejwt.exceptions import TokenError
-from rest_framework.response import Response
-from django.utils.translation import gettext_lazy as _
-
 from allauth.socialaccount.models import SocialAccount
+from .models import User
+
+
 BASE_URL = 'http://localhost:8000/'
 GOOGLE_CALLBACK_URI = BASE_URL + 'accounts/google/callback/'
 KAKAO_CALLBACK_URI = BASE_URL + 'accounts/kakao/callback/'
@@ -128,6 +124,9 @@ def kakao_callback(request):
     profile_request = requests.get(
         "https://kapi.kakao.com/v2/user/me", headers={"Authorization": f"Bearer {access_token}"})
     profile_json = profile_request.json()
+    error = profile_json.get("error")
+    if error is not None:
+        raise JSONDecodeError(error)
     kakao_account = profile_json.get('kakao_account')
     """
     kakao_account에서 이메일 외에
@@ -140,7 +139,6 @@ def kakao_callback(request):
     Signup or Signin Request
     """
     try:
-        print("1")
         user = User.objects.get(email=email)
         # 기존에 가입된 유저의 Provider가 kakao가 아니면 에러 발생, 맞으면 로그인
         # 다른 SNS로 가입된 유저
@@ -161,7 +159,6 @@ def kakao_callback(request):
         return JsonResponse(accept_json)
     except User.DoesNotExist:
         # 기존에 가입된 유저가 없으면 새로 가입
-        print("2")
         data = {'access_token': access_token, 'code': code}
         accept = requests.post(
             f"{BASE_URL}accounts/kakao/login/finish/", data=data)
@@ -180,10 +177,6 @@ class KakaoLogin(SocialLoginView):
     callback_url = KAKAO_CALLBACK_URI
 
 
-class GithubException(Exception):
-    pass
-
-
 def github_login(request):
     client_id = getattr(settings, 'SOCIAL_AUTH_GITHUB_KEY')
     return redirect(
@@ -192,30 +185,66 @@ def github_login(request):
 
 
 def github_callback(request):
+    client_id = getattr(settings, 'SOCIAL_AUTH_GITHUB_CLIENT_ID')
+    client_secret = getattr(settings, 'SOCIAL_AUTH_GITHUB_SECRET')
+    code = request.GET.get('code')
+    """
+    Access Token Request
+    """
+    token_req = requests.post(
+        f"https://github.com/login/oauth/access_token?client_id={client_id}&client_secret={client_secret}&code={code}&accept=&json&redirect_uri={GITHUB_CALLBACK_URI}&response_type=code", headers={'Accept': 'application/json'})
+    token_req_json = token_req.json()
+    error = token_req_json.get("error")
+    if error is not None:
+        raise JSONDecodeError(error)
+    access_token = token_req_json.get('access_token')
+    print(access_token)
+    print(code)
+    """
+    Email Request
+    """
+    user_req = requests.get(f"https://api.github.com/user",
+                            headers={"Authorization": f"Bearer {access_token}"})
+    user_json = user_req.json()
+    error = user_json.get("error")
+    if error is not None:
+        raise JSONDecodeError(error)
+    # print(user_json)
+    email = user_json.get("email")
+    """
+    Signup or Signin Request
+    """
     try:
-        client_id = getattr(settings, 'SOCIAL_AUTH_GITHUB_CLIENT_ID')
-        client_secret = getattr(settings, 'SOCIAL_AUTH_GITHUB_SECRET')
-        code = request.GET.get('code')
-        token_request = requests.post(
-            f"https://github.com/login/oauth/access_token?client_id={client_id}&client_secret={client_secret}&code={code}&accept=&json&redirect_uri={GITHUB_CALLBACK_URI}&response_type=code", headers={'Accept': 'application/json'})
-        token_request_json = token_request.json()
-        error = token_request_json.get("error")
-        if error is not None:
-            raise GithubException()
-        access_token = token_request_json.get('access_token')
-        error = token_request_json.get("error")
-        if error is not None:
-            raise GithubException()
+        user = User.objects.get(email=email)
+        # 기존에 가입된 유저의 Provider가 github가 아니면 에러 발생, 맞으면 로그인
+        # 다른 SNS로 가입된 유저
+        social_user = SocialAccount.objects.get(user=user)
+        if social_user is None:
+            return JsonResponse({'err_msg': 'email exists but not social user'}, status=status.HTTP_400_BAD_REQUEST)
+        if social_user.provider != 'github':
+            return JsonResponse({'err_msg': 'no matching social type'}, status=status.HTTP_400_BAD_REQUEST)
+        # 기존에 github로 가입된 유저
         data = {'access_token': access_token, 'code': code}
-        accept = requests.post(f"{BASE_URL}accounts/github/login/finish/",
-                               headers={'Accept': 'application/json'}, data=data)
+        accept = requests.post(
+            f"{BASE_URL}accounts/github/login/finish/", data=data)
+        accept_status = accept.status_code
+        if accept_status != 200:
+            return JsonResponse({'err_msg': 'failed to signin'}, status=accept_status)
         accept_json = accept.json()
-        error = accept_json.get("error")
-        if error is not None:
-            raise GithubException()
-        return Response(accept_json)
-    except GithubException:
-        return redirect('/error')
+        accept_json.pop('user', None)
+        return JsonResponse(accept_json)
+    except User.DoesNotExist:
+        # 기존에 가입된 유저가 없으면 새로 가입
+        data = {'access_token': access_token, 'code': code}
+        accept = requests.post(
+            f"{BASE_URL}accounts/github/login/finish/", data=data)
+        accept_status = accept.status_code
+        if accept_status != 200:
+            return JsonResponse({'err_msg': 'failed to signup'}, status=accept_status)
+        # user의 pk, email, first name, last name과 Access Token, Refresh token 가져옴
+        accept_json = accept.json()
+        accept_json.pop('user', None)
+        return JsonResponse(accept_json)
 
 
 class GithubLogin(SocialLoginView):
